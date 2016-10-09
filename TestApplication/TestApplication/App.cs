449ca19c675bbe8d0f2a447cfe433b171a7b3760
cumig0t <cumigot@gmail.com>
2016-10-09 19:56:53 +0700

    Update

--- TestApplication/TestApplication/App.cs
+++ TestApplication/TestApplication/App.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace TestApplication
 {
@@ -12,19 +7,22 @@ namespace TestApplication
       public App()
       {
          // The root page of your application
-         MainPage = new ContentPage
-         {
-            Content = new StackLayout
-            {
-               VerticalOptions = LayoutOptions.Center,
-               Children = {
-                  new Label {
-                     HorizontalTextAlignment = TextAlignment.Center,
-                     Text = "Welcome to Xamarin Forms!"
-                  }
-               }
-            }
-         };
+
+         MainPage = new CalculatorCode();
+         
+         //MainPage = new ContentPage
+         //{
+         //   Content = new StackLayout
+         //   {
+         //      VerticalOptions = LayoutOptions.Center,
+         //      Children = {
+         //         new Label {
+         //            HorizontalTextAlignment = TextAlignment.Center,
+         //            Text = "Welcome to Xamarin Forms!"
+         //         }
+         //      }
+         //   }
+         //};
       }
 
       protected override void OnStart()
---
+++ TestApplication/TestApplication/CalculatorCode.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace TestApplication
+{
+   internal class CalculatorCode : Page
+   {
+   }
+}
